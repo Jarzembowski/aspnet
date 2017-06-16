767a7bbfb981738f8b5e3f8a9c2a94fce02ec453
Jarzembowski <jargemboski@gmail.com>
2017-06-16 15:04:58 -0300

    first commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: /Movies/random
+        public ActionResult Random()
+        {
+
+         //return View(movie);
+         //return Content("teste");
+         //return HttpNotFound();
+         // return new EmptyResult();
+         //return RedirectToAction("Index","Home", new {index = 1, name = "teste"});
+
+         var movie = new Movie() { Name= "The Godfather"};        
+         var costumers = new List<Customer>
+         {
+            new Customer{Id = 1, name= "Roberto"},
+            new Customer{Id = 2, name= "Pedro"},
+            new Customer{Id = 3, name= "João"},
+            new Customer{Id = 4, name= "Lucas"}
+         };
+
+         var viewModel = new RandowMovieViewModel
+         {
+            Movie = movie,
+            Costumers = costumers
+         };
+
+         return View(viewModel);
+
+        }
+
+      public ActionResult edit(int id) {
+
+         return Content("id=" + id);
+
+      }
+
+
+      public ActionResult Index(int? pageIndex, string sortBy) {
+
+         if (!pageIndex.HasValue)
+         {
+            pageIndex = 1;
+         }
+
+         if (String.IsNullOrWhiteSpace(sortBy))
+         {
+            sortBy = "name";
+         }
+
+         return Content(String.Format("Pageindex={0}&SortBy={1}", pageIndex, sortBy));
+
+
+      }
+
+      [Route("movies/released/{year}/{month:range(1,12)}")]
+      public ActionResult ByReleaseDate(int year, int month)
+      {
+         return Content(year + "/" + month);
+      }
+
+
+    }
+}
