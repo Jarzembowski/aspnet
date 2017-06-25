14e5038f6a79e424260e7b95da59aefd932a2823
Jarzembowski <jargemboski@gmail.com>
2017-06-25 17:28:07 -0300

    mensagem

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -32,7 +32,7 @@ namespace Vidly.Controllers
             //Eager loading - carrega os objetos relacionados ao objeto principal
             var customers = _context.Customers.Include(c => c.membershipType).ToList();
 
-            return View(customers);
+            return View();
         }
         
         public ActionResult Details(int id)
