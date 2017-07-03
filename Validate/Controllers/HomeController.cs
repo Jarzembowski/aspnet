820e7ea4eb114eee452326ea0a9e1cc238bfc690
Jarzembowski <jargemboski@gmail.com>
2017-07-03 11:44:57 -0300

    projeto validaçao

---
+++ Validate/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Validate.Models;
+
+namespace Validate.Controllers
+{
+   public class HomeController : Controller
+   {
+
+      #region Register  
+
+      //  
+      // GET: /Home/Register  
+      [AllowAnonymous]
+      public ActionResult Register()
+      {
+         return View();
+      }
+
+      //  
+      // POST: /Home/Register  
+      [HttpPost]
+      [AllowAnonymous]
+      [ValidateAntiForgeryToken]
+      public ActionResult Register(HomeViewModel model)
+      {
+       
+         // Info.  
+         return Content("OK");
+       
+      }
+
+      #endregion
+   }
+}
